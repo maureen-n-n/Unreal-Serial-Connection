8cb773f47e683a2bcc9c798956fd969dbf9e3e6f
maureen <79654365+maureen-n-n@users.noreply.github.com>
2026-07-15 15:23:17 -0700

    turns on led

--- Source/SerialConnection/SerialConnection.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SerialConnection.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SerialConnection, "SerialConnection" );
